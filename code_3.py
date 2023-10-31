a5ed508540196a00afb3f01e48bea2afca163ed0
NatalyBlack <140597696+NatalyBlack@users.noreply.github.com>
2023-10-31 16:02:52 +0100

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,30 @@
+def main():
+
+    for item in data:
+def generate_random_data():
+
+import random
+    main()
+
+    data = generate_random_data()
+
+        print(f"Random Number: {item}")
+if __name__ == "__main__":
+    return data
+    data = [random.randint(1, 100) for _ in range(10)]function main() {
+        console.log("Random Number: " + item);
+    for (let i = 0; i < 10; i++) {
+    const data = [];
+    data.forEach(item => {
+
+
+}
+
+        data.push(Math.floor(Math.random() * 100) + 1);
+    }
+function generateRandomData() {
+    return data;
+}
+    const data = generateRandomData();
+main();
+    });
